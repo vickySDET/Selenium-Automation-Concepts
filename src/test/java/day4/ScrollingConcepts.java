250f1c4f75e1ec4a21623ffec88d5235e77daa7d
vickySDET <vigneshmech060694@gmail.com>
2025-11-16 17:52:14 +0530

    scrolling page

---
+++ src/test/java/day4/ScrollingConcepts.java
@@ -0,0 +1,24 @@
+package day4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ScrollingConcepts {
+	
+	
+
+	public static void main(String[] args) {
+		
+		   WebDriverManager.chromedriver().setup();
+
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://google.com");
+
+	        driver.manage().window().maximize();
+		
+     
+	}
+
+}
